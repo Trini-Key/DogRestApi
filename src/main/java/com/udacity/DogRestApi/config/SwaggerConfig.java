cabdef6e1cee6d8da428a938c51fb02c257a360c
keironjerome <Keyjerome@gmail.com>
2021-01-15 20:42:54 -0500

    Moved my ApiResponses into the correct class. Had them in the config file need them in the controller. Should work as intended now.

--- src/main/java/com/udacity/DogRestApi/config/SwaggerConfig.java
+++ src/main/java/com/udacity/DogRestApi/config/SwaggerConfig.java
@@ -16,12 +16,6 @@ import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
-@ApiResponses(value = {
-        @ApiResponse(code=400, message = "This is a bad request, please follow the API documentation for the proper request format."),
-        @ApiResponse(code=401, message = "Due to security constraints, your access request cannot be authorised."),
-        @ApiResponse(code=500, message = "The server is down. Please make sure that the Dog microservice is running.")
-
-})
 public class SwaggerConfig {
     @Bean
     public Docket api(){
